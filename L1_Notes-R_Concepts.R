b60c3cf765c81eec6709b7b9acbff95201f22cb6
Cari-D-Lewis <cdl5261@utulsa.edu>
2022-09-01 16:11:42 -0500

    created an html

---
+++ L1_Notes-R_Concepts.R
@@ -0,0 +1,17 @@
+##### 1-Sep-22 Data Wrangling and Visualization #####
+##### Lecture 1 Notes on R Concepts #################
+#####################################################
+
+### Data Life Cycle
+# Colletion : can be hard copies, when data are generated in the field; DIGITIZE
+# Entry : usually in excel; check the data, plot; can identify errors and outliers;
+#         clean the data to correct errors; lock it down in some way that can't be 
+#         modified - the "one true" dataset
+# Analysis : keep records of analysis; track changes (using Git); lots of comments
+#         showing what was done and why
+# Results & Figures : create an R Markdown file that has the full workflow and final
+#         figures; the Markdown should flow from raw data to final result and figures
+# Publication & Deposition of data/code : make data and code easily accessible in a
+#         git repo or something similar
+#####################################################
+
